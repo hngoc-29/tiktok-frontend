8024a89f778270812184653f00b45acdd3a428bf
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2025-08-30 16:18:39 +0000

    push tam

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,7 +9,7 @@ import ProfileVideo from './ui/ProfileVideo';
 
 export default function Profile({ user, isMe = false }: { user: UserType, isMe: boolean }) {
     return (
-        <div>
+        <div  className={styles.wrapper}>
             <div className={styles.header}>
                 <h1 className={styles.profileHeader}>{user.fullname}</h1>
                 {isMe && <Link href='/settings' className={styles.settingIcon}>
